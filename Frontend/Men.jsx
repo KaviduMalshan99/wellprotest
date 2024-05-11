cf3e9f8f364a47ac7710b46377b1fdc37c3053a4
Kavidu M Kulathunga <154060942+KaviduMalshan99@users.noreply.github.com>
2024-05-11 12:09:11 +0530

    gt

--- Frontend/Men.jsx
+++ Frontend/Men.jsx
@@ -10,7 +10,6 @@ import Header from './Header/Header';
 import LOGOO from '../src/assets/logoorange.png';
 import { PropagateLoader } from 'react-spinners'; 
 import MenBag from '../src/assets/menbag.jpg'
-
 import { faShoppingCart, faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
